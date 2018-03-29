a1b2b78d431166ace129b3e8a7e70b06f9b75fbe
yuchengc <yuchengc@umich.edu>
2018-03-29 12:47:15 -0400

    fix error

--- SI539/Portfolio/js/main.js
+++ SI539/Portfolio/js/main.js
@@ -45,10 +45,21 @@ $( document ).ready(function() {
 	    var scroll_y=window.pageYOffset;
 	    if (scroll_y>window.innerHeight){
 	    	console.log('scrolled down');
-	    	nar.style.backgroundColor="rgba(0,0,0,0.9)";
+	    	nar.style.backgroundColor="rgba(0,0,0,0.5)";
 	    }
 	    else if (scroll_y<window.innerHeight){
-	    	nar.style.backgroundColor="rgba(255,255,255,0.5)";
+	    	nar.style.backgroundColor="rgba(255,255,255,0)";
 	    }
     };
+
+    $('.project_box').children("img").hover(
+    	function(){
+    		$(this).css("filter", "saturate(0%) blur(3px)");
+    		// $(this).css("filter", "blur(100px)");
+    		$(this).css("-webkit-filter", "saturate(0%) blur(3px)");
+    		console.log('hover img');
+    	},
+    	function(){
+    		$(this).css("-webkit-filter", "saturate(100%) blur(0px)");
+    		$(this).css("filter", "saturate(100%) blur(0px)");})
 });	
