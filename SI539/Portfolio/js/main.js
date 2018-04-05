49ee14c53641d5b26db22326b8a3a2a2582a62d3
yuchengc <yuchengc@umich.edu>
2018-04-04 21:35:36 -0400

    update ICE 6 and portfolio

--- SI539/Portfolio/js/main.js
+++ SI539/Portfolio/js/main.js
@@ -52,14 +52,62 @@ $( document ).ready(function() {
 	    }
     };
 
-    $('.project_box').children("img").hover(
+    $('.project_box').hover(
     	function(){
-    		$(this).css("filter", "saturate(0%) blur(3px)");
+    		$(this).find('img').css("filter", "saturate(0%) blur(3px) brightness(0.7)");
     		// $(this).css("filter", "blur(100px)");
-    		$(this).css("-webkit-filter", "saturate(0%) blur(3px)");
+    		$(this).find('img').css("-webkit-filter", "saturate(0%) blur(3px) brightness(0.70)");
     		console.log('hover img');
+    		$(this).find(".project_intro").css({"animation-name":"proj_intro_hover_in",
+    			"animation-duration":"0s", "background-color":"rgba(0,242,226,0.6)"});
+    		$(this).find(".project_intro").css({"-webkit-animation-name":"proj_intro_hover_in",
+    "-webkit-animation-duration":"0s","background": "linear-gradient(130deg, rgba(119,200,192,0.6) 0%, rgba(199,170,255,0.5) 100%)"}); // "background-color":"rgba(0,242,226,0.3)",
+    		$(this).find(".project_intro").css("visibility","visible");
     	},
     	function(){
-    		$(this).css("-webkit-filter", "saturate(100%) blur(0px)");
-    		$(this).css("filter", "saturate(100%) blur(0px)");})
+    		$(this).find('img').css("-webkit-filter", "saturate(100%) blur(0px)");
+    		$(this).find('img').css("filter", "saturate(100%) blur(0px)");
+    		// $(this).find(".project_intro").css({"background-color":"rgba(0,242,226,0)","visibility": "hidden;"});
+    		$(this).find(".project_intro").css({"background":"linear-gradient(130deg, rgba(119,200,192,0.6) 0%, rgba(199,170,255,0.5) 100%)","visibility": "hidden;"});
+    		$(this).find(".project_intro").css("visibility","hidden");
+    	})
+		  // $(this).find(".project_intro").css({"animation-name":"proj_intro_hover_out",
+    // "animation-duration":"0.2s", "background-color":""})
+    // 		$(this).find(".project_intro").css({"-webkit-animation-name":"proj_intro_hover_out",
+    // "-webkit-animation-duration":"0.2s", "background-color":""})
+		    
 });	
+
+//Window Load
+jQuery(window).load(function($) {
+    
+    /*Init Portfolio*/
+    var container = jQuery("#work-grid");
+    if (container.length > 0) {
+        container.isotope({
+            layoutMode: 'masonry',
+            transitionDuration: '0.7s',
+            columnWidth: 60
+        });
+    };
+
+    //Filter Portfolio
+    jQuery('a.filter').on('click', function() {
+        var to_filter = jQuery(this).attr('data-filter');
+        if (to_filter == 'all') {
+            container.isotope({
+                filter: '.mix'
+            });
+        } else {
+            container.isotope({
+                filter: '.' + to_filter
+            });
+        }
+    });
+
+    //Switch Classes portfolio
+    jQuery('.filter').on('click', function() {
+        jQuery('a.filter').removeClass('active');
+        jQuery(this).addClass('active');
+    });
+});
